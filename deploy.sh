9d9fe410388f66f463764bcd0e5adc8bc9a5f01d
vaporz <zzxx513@gmail.com>
2021-02-05 20:40:21 +0800

    update script

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,17 @@ if [ -z "$IMAGE" ] || [ -z "$TARGET" ] || [ -z "$PORT" ]; then
   printf "Usage:
     deploy.sh [image] [ip] [port]
 Example:
-    sh deploy.sh scratch_gui 192.168.123.123 8601:8601
+    sh deploy.sh scratch_gui 192.168.31.222 8601:8601
 "
 	exit 0
 fi
 
-echo "Building docker image..."
-docker build . -t "$IMAGE" --compress
-docker save -o "$IMAGE".tar.gz "$IMAGE"
+if [ ! -f "$IMAGE.tar.gz" ];
+then
+    echo "Building docker image..."
+    docker build . -t "$IMAGE" --compress
+    docker save -o "$IMAGE".tar.gz "$IMAGE"
+fi
 
 echo "xiaoxiao" | ssh -tt zx@$TARGET '
 [ ! -d /opt/soft/'"$IMAGE"' ] && sudo mkdir -p /opt/soft/'"$IMAGE"' && sudo chown -R zx:zx /opt'
@@ -27,9 +30,9 @@ scp -p -C ./"$IMAGE".tar.gz zx@$TARGET:/opt/soft/$IMAGE
 echo "Starting ScratchGUI..."
 echo "xiaoxiao" | ssh -tt zx@$TARGET '
 cd /opt/soft/'"$IMAGE"';
-sudo docker container stop $(docker container ls -aq -f "ancestor='"$IMAGE"'");
+sudo docker container stop $(sudo docker container ls -aq -f "ancestor='"$IMAGE"'");
 sudo docker load --input '"$IMAGE"'.tar.gz;
 sudo docker run -d -p '"$PORT"' '"$IMAGE"';
-sudo docker container rm $(docker container ls -aq -f "status=exited");
-sudo docker rmi $(docker images -f "dangling=true" -q --no-trunc);
+sudo docker container rm $(sudo docker container ls -aq -f "status=exited");
+sudo docker rmi $(sudo docker images -f "dangling=true" -q --no-trunc);
 '
